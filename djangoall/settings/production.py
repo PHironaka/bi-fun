c048961cd6330dc4c9c943011ec1638b896aa572
PHironaka <peterhironaka@mac.com>
2018-07-04 10:35:15 -0700

    removed debug

--- djangoall/settings/production.py
+++ djangoall/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com', 
